78158d93136e12060e9d90aac4529464ac20b1e4
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2023-01-07 12:15:36 +0700

    Update baifham2.c

--- baifham2.c
+++ baifham2.c
@@ -1,68 +1,14 @@
-#include "stdio.h"
-#include "math.h"
-int kiemtrachan_le ( int dayso[5]) {
-	int  i, tong = 0, tong2 ;
+#include<stdio.h>
 
-	
-	if ( dayso[i] % 2 == 0){
-			printf("\n la so chan");
-	} else {
-			printf("\n la so le ");
-		}
-	
-	for ( i = dayso[0]; i<=dayso[5];i++ )	{
-	
-		if ( i % 2 == 0){
-	
-			tong2 = tong + i;
-		
-		}
-		return tong2;
-	
-	}
-		
-		
-	}
-		
-	
-	
-	
-	
-	
-
-	
-
-
-
-int main () {
-	int dayso[5], tong = 0, i, n = 5,tong2;
-
-	printf ("\n hay nhap 5 so nguyen ");
-	
-	
-	// nhap so lieu theo i
-	for(i = 0; i < n; i++) {
-			printf("\n nhap vao so thu %d =",i + 1 );
-			scanf("%d", &dayso[i]);
-		}
-	// in so lieu ra man hinh
-	for(i = 0; i < 5; i++)
-    { 
-        printf("\na[%d] = %d \n", i, dayso[i]); 
-        printf("\n\n ",kiemtrachan_le(dayso));
-		
-    }
-    // tinh tong gia tri cua cac phan tu trong mang 
-    for(i = 0; i < 5; i++) 
-        tong = tong + dayso[i]; 
-    printf("\ntong = %d",  tong); 
-    printf ("\ntong cac so chan = %d", tong2);
-    
-	
-	
-	
-	
-
-
-
+void hoanvi(int *x, int *y);
+int main() {
+    int x = 3, y = 5;
+    printf("Truoc khi hoan vi: x = %d y = %d\n", x, y);
+    hoanvi(&x, &y);
+    printf("Sau khi hoan vi: x = %d y = %d", x, y);
+}
+void hoanvi(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
